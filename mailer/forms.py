29891c8abab3f4b3a3e982a8e7279006b35a4d72
Daria_Makurova <dariamakurova@gmail.com>
2026-04-18 23:38:16 +0300

    forms created

--- mailer/forms.py
+++ mailer/forms.py
@@ -1,13 +1,51 @@
-from django.forms import ModelForm
+from django import forms
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
-from mailer.models import Client
+from .models import Client, Message, Mailing
 
 
-class ClientForm(ModelForm):
+class ClientForm(forms.ModelForm):
+    """Форма для создания и редактирования клиента"""
     class Meta:
         model = Client
+        fields = ['email', 'name', 'comment']
 
-
-class MessageForm(ModelForm):
+class MessageForm(forms.ModelForm):
+    """Форма для создания и редактирования сообщения"""
     class Meta:
         model = Message
+        fields = ['title', 'body']
+
+
+class MailingForm(forms.ModelForm):
+    """Форма для создания и редактирования рассылки"""
+
+    class Meta:
+        model = Mailing
+        fields = ['start_date', 'end_date', 'message', 'recipients']
+        widgets = {
+            'start_date': forms.DateTimeInput(attrs={'type': 'datetime-local', 'class': 'form-control'}),
+            'end_date': forms.DateTimeInput(attrs={'type': 'datetime-local', 'class': 'form-control'}),
+            'message': forms.Select(attrs={'class': 'form-control'}),
+            'recipients': forms.SelectMultiple(attrs={'class': 'form-control', 'size': 10}),
+        }
+
+    def clean(self):
+        """Валидация дат"""
+        cleaned_data = super().clean()
+        start_date = cleaned_data.get('start_date')
+        end_date = cleaned_data.get('end_date')
+
+        if start_date and end_date:
+            if end_date <= start_date:
+                raise ValidationError(
+                    'Дата окончания должна быть позже даты начала рассылки'
+                )
+
+            if end_date < timezone.now():
+                raise ValidationError(
+                    'Дата окончания не может быть в прошлом'
+                )
+
+        return cleaned_data
